9546a9920b5ab0ceeaf2abe8c9f31c53deb24b42
Chandrashekhar19 <112744043+Chandrashekhar19@users.noreply.github.com>
2022-09-05 19:00:44 +0530

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,res;
+	printf("Enter value for a:");
+	scanf("%d",&a);
+	printf("Enter value for b:");
+	scanf("%d",&b);
+	res=a+b;
+	printf("The %d and %d Addition is %d\n",a,b,res);
+	return 0;
+}
+	
